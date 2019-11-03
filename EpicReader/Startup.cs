a63d6e9168d7b1eb35e4efa6b56172d7b77c9757
Piotr Cieślik <piotr.cieslik@outlook.com>
2019-11-03 13:04:18 +0100

    Temporary disable HTTPS

--- EpicReader/Startup.cs
+++ EpicReader/Startup.cs
@@ -32,13 +32,12 @@ namespace EpicReader
             {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                // app.UseHsts();
             }
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
-
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>

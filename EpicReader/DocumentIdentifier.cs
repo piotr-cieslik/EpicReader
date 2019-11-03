16e9fa705b860f367ec78143c56dea8e31649907
Piotr Cieślik <piotr.cieslik@outlook.com>
2019-11-03 12:06:16 +0100

    Add types DocumentIdentifier and TemporaryDocumentIdentifier

---
+++ EpicReader/DocumentIdentifier.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace EpicReader
+{
+    public sealed class DocumentIdentifier
+    {
+        private readonly DateTime _timestamp;
+        private readonly string _fileName;
+
+        public DocumentIdentifier(
+            DateTime timestamp,
+            string fileName)
+        {
+            _timestamp = timestamp;
+            _fileName = fileName;
+        }
+
+        public DateTime Timestamp() => _timestamp;
+
+        public string FileName() => _fileName;
+
+        public static DocumentIdentifier Parse(string documentIdentifier)
+        {
+            var ticks = long.Parse(documentIdentifier.Substring(0, 20));
+            var timestamp = new DateTime(ticks);
+            var fileName = documentIdentifier.Substring(21, documentIdentifier.Length - 21);
+            return new DocumentIdentifier(timestamp, fileName);
+        }
+
+        public override string ToString()
+        {
+            return $"{_timestamp.Ticks.ToString("D20")}-{_fileName}";
+        }
+    }
+}
---
+++ EpicReader/TemporaryDocumentIdentifier.cs
@@ -0,0 +1,11 @@
+﻿namespace EpicReader
+{
+    public sealed class TemporaryDocumentIdentifier
+    {
+        private readonly DocumentIdentifier _documentIdentifier;
+
+        public TemporaryDocumentIdentifier(DocumentIdentifier documentIdentifier) => _documentIdentifier = documentIdentifier;
+
+        public override string ToString() => _documentIdentifier.ToString() + ".tmp";
+    }
+}

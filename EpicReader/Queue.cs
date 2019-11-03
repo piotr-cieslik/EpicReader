87d056b226c54164f4f6406784a2edd84af2c8a0
Piotr Cieślik <piotr.cieslik@outlook.com>
2019-11-03 11:30:36 +0100

    Add queue class

---
+++ EpicReader/Queue.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace EpicReader
+{
+    internal sealed class Queue
+    {
+        private Storage _storage;
+
+        public Queue()
+        {
+            _storage = new Storage();
+        }
+
+        public async Task<string> Put(string fileName, Stream stream)
+        {
+            var temporaryFileName = fileName + ".tmp";
+            await _storage.WriteFileAsync(temporaryFileName, stream);
+            _storage.RenameFile(temporaryFileName, fileName);
+            return string.Empty;
+        }
+
+        public IEnumerable<string> List() => _storage.GetFiles();
+    }
+}

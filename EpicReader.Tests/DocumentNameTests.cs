2dc18bf47b33a71e6fcd1dc4174d9e5621b0f321
Piotr Cieślik <piotr.cieslik@outlook.com>
2019-11-04 19:52:58 +0100

    Add type DocumentName

---
+++ EpicReader.Tests/DocumentNameTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xunit;
+
+namespace EpicReader.Tests
+{
+    public class DocumentNameTests
+    {
+        [Fact]
+        public void ShouldConvertToString()
+        {
+            var timestamp = new Timestamp(123456789);
+            var guid = new Guid("a9d7a3ae-df80-4925-a542-17347df3a280");
+            var fileName = new FileName("file.pdf");
+            var documentName = new DocumentName(timestamp, guid, fileName);
+
+            Assert.Equal(
+                "0123456789_a9d7a3aedf804925a54217347df3a280_file.pdf",
+                documentName.ToString());
+        }
+    }
+}
---
+++ EpicReader/DocumentName.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EpicReader
+{
+    public sealed class DocumentName
+    {
+        private readonly Timestamp _timestamp;
+        private readonly Guid _guid;
+        private readonly FileName _fileName;
+
+        public DocumentName(
+            Timestamp timestamp,
+            Guid guid,
+            FileName fileName)
+        {
+            _timestamp = timestamp;
+            _guid = guid;
+            _fileName = fileName;
+        }
+
+        public override string ToString() => $"{_timestamp.ToString()}_{_guid.ToString().Replace("-", string.Empty)}_{_fileName}";
+    }
+}

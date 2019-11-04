b9bd057a7a4105a7ac60dd395c35ed220b1b2f93
Piotr Cieślik <piotr.cieslik@outlook.com>
2019-11-04 18:34:43 +0100

    Add timestamp with tests

---
+++ EpicReader.Tests/TimestampTests.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+
+namespace EpicReader.Tests
+{
+    public class TimestampTests
+    {
+        [Theory]
+        [InlineData(0, "0000000000")]
+        [InlineData(1, "0000000001")]
+        [InlineData(1572888075, "1572888075")]
+        public void ShouldBeConvertedToFixedLengthString(long value, string expected)
+        {
+            Assert.Equal(
+                expected,
+                new Timestamp(value).ToString());
+        }
+
+        [Fact]
+        public void ShouldBeEqual()
+        {
+            Assert.Equal(
+                new Timestamp(0),
+                new Timestamp(new System.DateTime(1970, 1, 1, 0, 0, 0, System.DateTimeKind.Utc)));
+        }
+    }
+}
---
+++ EpicReader/Timestamp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace EpicReader
+{
+    public sealed class Timestamp 
+        : IEquatable<Timestamp>
+    {
+        private readonly long _value;
+
+        public Timestamp(DateTime value)
+            : this(((DateTimeOffset)value).ToUnixTimeSeconds())
+        {
+        }
+
+        public Timestamp(long value) => _value = value;
+
+        public override bool Equals(object obj) => Equals(obj as Timestamp);
+
+        public bool Equals([AllowNull] Timestamp other) => other != null && _value == other._value;
+
+        public override int GetHashCode() => HashCode.Combine(_value);
+
+        public override string ToString() => _value.ToString("D10");
+
+        public static bool operator ==(Timestamp left, Timestamp right) => EqualityComparer<Timestamp>.Default.Equals(left, right);
+
+        public static bool operator !=(Timestamp left, Timestamp right) => !(left == right);
+    }
+}

45fc8ed87173d6d8f92ba76f64a0b7168582af68
Piotr Cieślik <piotr.cieslik@outlook.com>
2019-11-05 20:57:46 +0100

    Add Result type

---
+++ EpicReader/Result.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace EpicReader
+{
+    public sealed class Result
+    {
+        public Result(
+            string status,
+            string text,
+            DateTime processingStartTime,
+            DateTime processingEndTime)
+        {
+            Status = status;
+            Text = text;
+            ProcessingStartTime = processingStartTime;
+            ProcessingEndTime = processingEndTime;
+        }
+
+        public string Status { get; }
+
+        public string Text { get; }
+
+        public DateTime ProcessingStartTime { get; }
+
+        public DateTime ProcessingEndTime { get; }
+    }
+}
